9a0caabfe0cd7af97a622f44660a7fcba8259920
Daniel Fernandez Rabal <Daniel@daniels-MacBook-Air.local>
2018-02-05 19:35:32 +0400

    Fastest route algorithm and build script modification

--- src/app/components/search-result/search-result.component.ts
+++ src/app/components/search-result/search-result.component.ts
@@ -40,7 +40,7 @@ export class SearchResultComponent implements OnInit {
       this.calculateCheapestRoute();
     }
     else if (this.searchData.filter == 'fastest') {
-
+      this.calculatefastestRoute();
     }
   }
 
@@ -148,7 +148,7 @@ export class SearchResultComponent implements OnInit {
         directTripTotalCost = directTrip.cost - ((directTrip.cost * directTrip.discount) / 100);
         directTripTotalDuration = directTrip.duration;
 
-        if (directTripTotalCost < indirectTripTotalCost) {
+        if (directTripTotalCost <= indirectTripTotalCost) {
           this.results = [];
           this.results.push(directTrip);
           this.tripTotalCost = directTripTotalCost;
@@ -159,8 +159,123 @@ export class SearchResultComponent implements OnInit {
 
   }
 
-  fastestRouteAlgorithm() {
+  calculatefastestRoute() {
+
+    let directTrip: Deal;
+    let directTripTotalCost: number;
+    let indirectTripTotalCost: number;
+    let directTripTotalDuration: Duration;
+    let indirectTripTotalDuration: Duration;
+
+    let deals: Array<Deal>;
+    let dealsFiltered: Array<Deal> = new Array<Deal>();
+
+    //let arrayCostsDiscounted: Array<number> = new Array<number>();
+    //let minArrayCostDiscounted: number;
     
+    let arrayDurations: Array<number> = new Array<number>();
+    let minArrayDuration: number;
+
+    let source;
+    let filterObservable;
+    let currentDeparture;
+
+    let loopCounter: number;
+    let stopWhileLoop: boolean;
+
+    // variable Initializations
+    directTripTotalCost = 0;
+    indirectTripTotalCost = 0;
+
+    directTripTotalDuration = new Duration();
+    directTripTotalDuration.h = 0;
+    directTripTotalDuration.m = 0;
+
+    indirectTripTotalDuration = new Duration();
+    indirectTripTotalDuration.h = 0;
+    indirectTripTotalDuration.m = 0;
+    loopCounter = 0;
+    stopWhileLoop = false;
+    this.showTripWarning = false;
+    deals = this.deals;
+    currentDeparture = this.searchData.departure;
+
+    if (this.searchData.departure == this.searchData.arrival) {
+      this.showTripWarning = true;
+    }
+    else {
+      
+      // Calculating indirect route using Dijkstra algorithm concept
+      while (currentDeparture !== this.searchData.arrival && loopCounter < this.deals.length && !stopWhileLoop) {
+
+        /*
+          - Filtering deals by departure
+          - Calculating an array of durations of this filtered array
+          - Calculating the minimun value of this durations array for the next filter
+        */
+
+        dealsFiltered = this.filterDealsByDeparture(deals, currentDeparture);
+        arrayDurations = this.calculateArrayDurations(dealsFiltered);
+        minArrayDuration = this.getMin(arrayDurations);
+
+        /* 
+          - Filtering previus filtered array with the minimun duration
+        */
+        dealsFiltered = this.filterDealsByMinDuration(dealsFiltered, minArrayDuration);
+
+        // To avoid looping trips (trips that are running in circles)
+        if (!this.results.includes(dealsFiltered[0])) {
+
+          /*
+            - Removing way back of current deal filtered selected from deals array
+            - Adding current deal selected to results array
+            - Changing current departure to next one
+            - Calculating total trip's cost and duration
+            - Clearing arrays
+          */
+
+          deals = this.removeWayBackDeal(deals, dealsFiltered[0]);
+
+          this.results.push(dealsFiltered[0]);
+          currentDeparture = dealsFiltered[0].arrival;
+          indirectTripTotalCost += (dealsFiltered[0].cost - ((dealsFiltered[0].cost * dealsFiltered[0].discount) / 100));
+
+          indirectTripTotalDuration.h += dealsFiltered[0].duration.h * 1;
+          indirectTripTotalDuration.m += dealsFiltered[0].duration.m * 1;
+
+          dealsFiltered = [];
+          //arrayCostsDiscounted = [];
+          arrayDurations = [];
+        }
+        else {
+          this.results = [];
+          indirectTripTotalCost = 0;
+          stopWhileLoop = true;
+          this.showTripWarning = true;
+        }
+
+        loopCounter++;
+      }
+
+      this.tripTotalCost = indirectTripTotalCost;
+      this.tripTotalDuration = this.getAdjustedDuration(indirectTripTotalDuration);
+
+      //After calculating the indirect route if there is a direct route with less cost, this one is choosen as result
+      if (this.isDirectTrip(deals)) {
+
+        directTrip = this.getFastestDirectTrip(this.deals);
+        directTripTotalCost = directTrip.cost - ((directTrip.cost * directTrip.discount) / 100);
+        directTripTotalDuration = directTrip.duration;
+
+        if (directTripTotalDuration <= indirectTripTotalDuration) {
+          this.results = [];
+          this.results.push(directTrip);
+          this.tripTotalCost = directTripTotalCost;
+          this.tripTotalDuration = directTripTotalDuration;
+        }
+      }
+    }
+
   }
 
   // Function to get a filtered array of deals by departure city
@@ -226,7 +341,7 @@ export class SearchResultComponent implements OnInit {
 
     source = Observable.from(deals);
 
-    filterObservable = source.filter(deal => parseInt(deal.duration.h) * 60 + parseInt(deal.duration.m) == minDuration);
+    filterObservable = source.filter(deal => (deal.duration.h * 60) + (deal.duration.m * 1) == minDuration); 
 
     filterObservable.subscribe(val => {
       dealsFiltered.push(val);
@@ -253,7 +368,7 @@ export class SearchResultComponent implements OnInit {
     let arrayDurations: Array<number> = new Array<number>();
 
     for (let deal of deals) {
-      arrayDurations.push((deal.duration.h * 60) + (deal.duration.m));
+      arrayDurations.push((deal.duration.h * 60) + (deal.duration.m * 1));
     }
 
     return arrayDurations;
@@ -267,10 +382,10 @@ export class SearchResultComponent implements OnInit {
   // Funcion to get proper adjusted duration
   getAdjustedDuration(duration : Duration): Duration{
     
-    let hoursToAdd : number= 0;
+    let hoursToAdd : number = 0;
     let restMin : number = 0;
 
-    if(duration.m > 60){
+    if(duration.m >= 60){
       hoursToAdd = Math.trunc(duration.m / 60);
       restMin = duration.m % 60;
       duration.h += hoursToAdd;
@@ -310,6 +425,21 @@ export class SearchResultComponent implements OnInit {
     minArrayCostDiscounted = this.getMin(this.calculateArrayCostDiscounted(dealsFiltered));
 
     dealsFiltered = this.filterDealsByMinCostDiscounted(dealsFiltered, minArrayCostDiscounted);
+
+    return dealsFiltered[0];
+  }
+
+  // Function to get cheapest direct trip
+  getFastestDirectTrip(deals: Array<Deal>): Deal {
+
+    let dealsFiltered: Array<Deal>;
+    let minArrayDurations: number;
+
+    dealsFiltered = this.filterDealsByDepartureAndArrival(deals, this.searchData.departure, this.searchData.arrival);
+
+    minArrayDurations = this.getMin(this.calculateArrayDurations(dealsFiltered));
+
+    dealsFiltered = this.filterDealsByMinDuration(dealsFiltered, minArrayDurations);
 
     return dealsFiltered[0];
   }
